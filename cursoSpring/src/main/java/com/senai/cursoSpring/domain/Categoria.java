2b65842eb64d88c892734404d7cb64c245d9390d
rafaelalvesmartins <rafaelmartinsalves@gmail.com>
2020-09-13 14:20:12 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- cursoSpring/src/main/java/com/senai/cursoSpring/domain/Categoria.java
+++ cursoSpring/src/main/java/com/senai/cursoSpring/domain/Categoria.java
@@ -2,16 +2,25 @@ package com.senai.cursoSpring.domain;
 
 import java.io.Serializable;
 
-public class Categoria implements Serializable{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Categoria implements Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-	
+
 	public Categoria() {
-		
+
 	}
 
 	public Categoria(Integer id, String nome) {
@@ -60,6 +69,5 @@ public class Categoria implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
+
 }
