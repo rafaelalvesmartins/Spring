550cfb23356cb0468dd6237a0db3a15d45dc35b8
rafaelalvesmartins <rafaelmartinsalves@gmail.com>
2020-09-20 10:46:08 -0300

    Produto e associacao muitos para muitos

---
+++ cursoSpring/src/main/java/com/senai/cursoSpring/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.senai.cursoSpring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.senai.cursoSpring.domain.Produto;
+
+@Repository
+public interface  ProdutoRepository extends JpaRepository<Produto,Integer>{
+
+}
