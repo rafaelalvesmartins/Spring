d53bf87b39a33918b6fabff2d3597b79b64fda97
rafaelalvesmartins <rafaelmartinsalves@gmail.com>
2020-09-27 10:53:15 -0300

    Adicionando Listar Tudo de Categoria

--- cursoSpring/src/main/java/com/senai/cursoSpring/resources/CategoriaResource.java
+++ cursoSpring/src/main/java/com/senai/cursoSpring/resources/CategoriaResource.java
@@ -1,10 +1,10 @@
 package com.senai.cursoSpring.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +15,7 @@ import com.senai.cursoSpring.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CategoriaResource {
 	
 	@Autowired
@@ -24,5 +25,11 @@ public class CategoriaResource {
 	public ResponseEntity<?> listar(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> listarTudo() {
+		return service.retornaTudo();
 	}
 }
