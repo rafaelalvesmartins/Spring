53bafc8035efaec0920102748135c5aec2153c26
rafaelalvesmartins <rafaelmartinsalves@gmail.com>
2020-10-04 14:24:06 -0300

    Cliente, TipoCliente, telefones e endereços - Parte 2

--- cursoSpring/src/main/java/com/senai/cursoSpring/CursoSpringApplication.java
+++ cursoSpring/src/main/java/com/senai/cursoSpring/CursoSpringApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.senai.cursoSpring.domain.Categoria;
 import com.senai.cursoSpring.domain.Cidade;
+import com.senai.cursoSpring.domain.Cliente;
+import com.senai.cursoSpring.domain.Endereco;
 import com.senai.cursoSpring.domain.Estado;
 import com.senai.cursoSpring.domain.Produto;
+import com.senai.cursoSpring.domain.enums.TipoCliente;
 import com.senai.cursoSpring.repositories.CategoriaRepository;
 import com.senai.cursoSpring.repositories.CidadeRepository;
+import com.senai.cursoSpring.repositories.ClienteRepository;
+import com.senai.cursoSpring.repositories.EnderecoRepository;
 import com.senai.cursoSpring.repositories.EstadoRepository;
 import com.senai.cursoSpring.repositories.ProdutoRepository;
 
@@ -27,6 +32,10 @@ public class CursoSpringApplication implements CommandLineRunner {
 	private CidadeRepository cidadeRepository;
 	@Autowired
 	private EstadoRepository estadoRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	
 	
@@ -67,6 +76,16 @@ public class CursoSpringApplication implements CommandLineRunner {
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com","36378912377",TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363326","93838393"));
+		
+		Endereco e1 = new Endereco(null,"Rua Flores","300","Apto 303","Jardim","38220834",cli1,c1);
+		Endereco e2 = new Endereco(null,"Avenida Matos","105","Sala 800","Centro","38777012",cli1,c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 		
 	}
 
---
+++ cursoSpring/src/main/java/com/senai/cursoSpring/repositories/EnderecoRepository.java
@@ -0,0 +1,13 @@
+package com.senai.cursoSpring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.senai.cursoSpring.domain.Endereco;
+
+
+
+@Repository
+public interface  EnderecoRepository extends JpaRepository<Endereco,Integer>{
+
+}
