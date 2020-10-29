453683054eed988297a50b1a424444d88433def8
rafaelalvesmartins <rafaelmartinsalves@gmail.com>
2020-10-29 17:25:32 -0300

    Endpoint /clientes/{id} disponível

--- cursoSpring/src/main/java/com/senai/cursoSpring/resources/ProdutoResource.java
+++ cursoSpring/src/main/java/com/senai/cursoSpring/resources/ProdutoResource.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,6 +21,7 @@ import com.senai.cursoSpring.services.ProdutoService;
 
 @RestController
 @RequestMapping(value = "/produtos")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProdutoResource {
 	
 	@Autowired
